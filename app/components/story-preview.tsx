e20cb971e6caaf1d20b7b2160cb8f8d59d81aeb5
TimothyYunPan <s093261s@gmail.com>
2025-04-19 01:26:02 +0800

    adjust api route

--- app/components/story-preview.tsx
+++ app/components/story-preview.tsx
@@ -155,7 +155,7 @@ export default function StoryPreview({ setStoryStep, completedStory, storyId }: 
     }
 
     try {
-      const response = await fetch('/api/proxy/api/v1/epson/print_picture_book', {
+      const response = await fetch('/api/proxy/api/v1/picture_book/print_picture_book', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
